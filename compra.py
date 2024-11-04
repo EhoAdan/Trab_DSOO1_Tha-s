2f4cf0ff7f348e2427541d4acd97ef4731b948ca
EhoAdan <adansamuelpruss@gmail.com>
2024-11-04 20:12:24 -0300

    Update compra.py
    Update compra

--- compra.py
+++ compra.py
@@ -1,3 +1,17 @@
+from item import Item
+from datetime import datetime
+
 class Compra:
-    pass
+
+    def __init__(self, item: Item):
+        self.__item = item
+        self.__data = datetime.today().strftime("%m-%Y")
+    
+    @property
+    def item(self):
+        return self.__item
+    
+    @property
+    def data(self):
+        return self.__data
 # Placeholder pro import compra não reclamar
